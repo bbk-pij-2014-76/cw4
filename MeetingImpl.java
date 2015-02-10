95e685d93878f2a97042101fb77dd368e7a5c072
BBK-PIJ-2014-76 <syed.maqsood.ali@live.com>
2015-02-10 22:36:22 +0000

    MeetingImpl.java was added along with constructor and code was added for the methods

---
+++ MeetingImpl.java
@@ -0,0 +1,27 @@
+import java.util.Calendar;
+import java.util.Set;
+public class MeetingImpl implements Meeting
+{
+	private int id;
+	private Calendar date;
+	private Set<Contact> contactList;
+
+	public MeetingImpl(int id, Set<Contact> contacts, Calendar date)
+	{
+		this.id = id;
+		this.contactList = contacts;
+		this.date = date;
+	}
+	public int getId()
+	{
+		return id;
+	}
+	public Calendar getDate()
+	{
+		return date;
+	}
+	public Set<Contact> getContacts()
+	{
+		return contactList;
+	}
+}
