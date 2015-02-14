360c5f169c14cbb882cb00f9cf4a4f9e75ae4117
BBK-PIJ-2014-76 <syed.maqsood.ali@live.com>
2015-02-14 20:13:02 +0000

    test file was added called MeetingImplTest.java

---
+++ MeetingImplTest.java
@@ -0,0 +1,24 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashSet;
+import java.util.Set;
+public class MeetingImplTest
+{
+	private Meeting meeting;
+	private Set<Contact> contactList;
+
+	@Before
+	public void buildUp()
+	{
+		Contact michael = new ContactImpl(5, "Michael", "plays tennis");
+		Contact farah = new ContactImpl(7, "Farah", "likes cricket");
+		contactList = new HashSet<Contact>();
+		contactList.add(michael);
+		contactList.add(farah);
+		Calendar calendar = new GregorianCalendar(2014,05,03);
+		meeting = new MeetingImpl(678, contactList, calendar);
+	}
+
+}
